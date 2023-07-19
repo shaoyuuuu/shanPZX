73254a5bfb002f73a905cfe4d237ae0b31c41402
170 <sy170666@qq.com>
2023-07-19 14:50:35 +0800

    hosky test

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,10 @@
   </div>
 </template>
 
-<script setup lang='ts'>
-for(let i =0;i<100;i++){
-  console.log(i);
-  
+<script setup lang="ts">
+for (let i = 0; i < 100; i++) {
+  console.log(i)
 }
 </script>
 
-<style scoped>
-</style>
- 
+<style scoped></style>
